63b0121a3867bdeb763e06ef8ba27f363b532cfc
ahmad01-git <shuhaibqayumi@gmail.com>
2026-04-30 10:02:23 +0200

    connect backend to neon database

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const pool = require('../db/connect');
 const express = require('express');
 const app = express();
 
@@ -11,4 +12,14 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server kører på http://localhost:${PORT}`);
+});
+
+app.get('/api/db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('DB fejl');
+  }
 });
--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,15 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const { Pool } = pg
+require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-})
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-export default pool
+module.exports = pool;
