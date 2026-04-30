0ec779a30a5f224996a1d3725dc98384a7b2982a
ahmad01-git <shuhaibqayumi@gmail.com>
2026-04-30 09:39:42 +0200

    min ændring

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
-// Sæt din Express server op her
 const express = require('express');
 const app = express();
 
-// ...
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server virker!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server kører på http://localhost:${PORT}`);
+});
